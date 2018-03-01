1e2129442e6b103b0e41908c26ca9d8d99437756
LeAnnS <leann.sawatzky@gmail.com>
2018-03-01 03:20:37 -0600

    posting to api routes and functions in process

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,11 @@
 var friendData = require("../data/friends.js");
 
 module.exports = function(app) {
-	app.get("/api/friends", function (req, res) {
+	app.get("/api/friends", function(req, res) {
 		res.json(friendData);
+	});
+	app.post("/api/friends", function(req, res) {
+		friendData.push(req.body);
+		res.json(userData);
 	});
 }
